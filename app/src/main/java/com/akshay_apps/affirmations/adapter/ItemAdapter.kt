815c7b0826208de0de9d2cbee60b110481569519
Akshay Khare <akhare931@gmail.com>
2021-05-21 01:09:35 +0530

    Added Images in Recycler View

--- app/src/main/java/com/akshay_apps/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/akshay_apps/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.akshay_apps.affirmations.R
@@ -24,6 +25,7 @@ class ItemAdapter(private val context: Context,
 
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -45,6 +47,7 @@ class ItemAdapter(private val context: Context,
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataSet[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
